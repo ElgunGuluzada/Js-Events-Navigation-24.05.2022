a974da37ad321808801e08256094a59791535c3d
Elgun Guluzada <elgunpg@code.edu.az>
2022-05-25 01:07:20 +0400

    feat: some changes task 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,36 @@
-let box = document.getElementById("box")
+let mouse = document.getElementById("mouse")
 let marginX = 0;
 let marginY = 0;
+let width = 100;
 
 document.addEventListener('keydown', function (e) {
     switch (e.keyCode) {
         case 37:
             marginX = marginX - 50;
-            box.style.background = "red"
-            box.style.marginLeft = `${marginX}px`
+            mouse.style.marginLeft = `${marginX}px`;
+
+            mouse.style.transform = "rotate(-20deg)";
+
             break;
         case 38:
             marginY = marginY - 50;
-            box.style.background = "blue"
-            box.style.marginTop = `${marginY}px`
+            mouse.style.marginTop = `${marginY}px`
+            width = width - 10;
+            mouse.style.width = `${width}px`
+            mouse.style.transform = "rotate(0deg)";
             break;
         case 39:
             marginX = marginX + 50;
-            box.style.background = "green"
-            box.style.marginLeft = `${marginX}px`
+            mouse.style.marginLeft = `${marginX}px`
+            mouse.style.transform = "rotate(20deg)";
             break;
         case 40:
             marginY = marginY + 50;
-            box.style.background = "yellow"
-            box.style.marginTop = `${marginY}px`
+            mouse.style.marginTop = `${marginY}px`
+            width = width + 10;
+            mouse.style.width = `${width}px`
+            mouse.style.transform = "rotate(0deg)";
+
             break;
     }
 });
